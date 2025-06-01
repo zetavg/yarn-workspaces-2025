316cf0268a2bf5cf93f240cf60a84e2e8fd504f7
Pokai Chang <mail@zeta.vg>
2025-06-01 17:12:38 +0800

    update eslint-config

--- configs/eslint-config/presets/default.js
+++ configs/eslint-config/presets/default.js
@@ -1,3 +1,4 @@
+import vitest from '@vitest/eslint-plugin';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
@@ -7,6 +8,7 @@ import * as configs from '../configs/index.js';
 export default defineConfig([
   { ignores: ['dist', 'node_modules'] },
   tseslint.configs.recommended,
+  vitest.configs.recommended,
   {
     name: 'Configs Applied to All JS/TS Files',
     files: ['**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
