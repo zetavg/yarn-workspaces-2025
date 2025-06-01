0cb56d959ef914a1a7848e40049c56f1809c099e
Pokai Chang <mail@zeta.vg>
2025-06-01 17:45:36 +0800

    remove jest-config

--- configs/jest-config/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  root: true,
-  extends: ['@yarn-workspaces-2025'],
-};
-
-module.exports = config;
--- configs/jest-config/index.ts
+++
@@ -1,1 +0,0 @@
-export { jestBaseConfig } from './jestBaseConfig';
